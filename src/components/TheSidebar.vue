1b29bccab69291fadf09d2547acec63a4166f2a8
jayon <jayon.niravel@aeropay.com>
2025-04-08 14:47:51 -0500

    Added Embedded page

--- src/components/TheSidebar.vue
+++ src/components/TheSidebar.vue
@@ -1,8 +1,24 @@
 <template>
-    <aside class="bg-gray-300 dark:bg-gray-800 text-gray-900 dark:text-white p-4">
-        <ul class="mt-8">
-        <li><RouterLink to="/home" block py-2 px-4 hover:bg-gray-100 dark:hover:bg-gray-700>Home</RouterLink></li>
-        <li><RouterLink to="/about" block py-2 px-4 hover:bg-gray-100 dark:hover:bg-gray-700>About</RouterLink></li>
+    <aside bg-gray-300 dark:bg-gray-800 text-gray-900 dark:text-white p-4 w-2xs w-48>
+        <ul mt-8>
+            <li>
+                <div grid="~ flow-col cols-[auto_1fr]">
+                    <div place-self-center i-carbon:home />
+                    <RouterLink to="/home" block py-2 px-4 hover:bg-gray-100 dark:hover:bg-gray-700 text-xl>Home</RouterLink>
+                </div>
+            </li>
+            <li>
+                <div grid="~ flow-col cols-[auto_1fr]">
+                    <div place-self-center i-carbon:help-filled />
+                    <RouterLink to="/about" block py-2 px-4 hover:bg-gray-100 dark:hover:bg-gray-700 text-xl>About</RouterLink>
+                </div>
+            </li>
+            <li>
+                <div grid="~ flow-col cols-[auto_1fr]">
+                    <div place-self-center i-carbon:3rd-party-connected />
+                    <RouterLink to="/embedded" block py-2 px-4 hover:bg-gray-100 dark:hover:bg-gray-700 text-xl>Embedded</RouterLink>
+                </div>
+            </li>
         </ul>
     </aside>
 </template>
---
+++ src/pages/embedded.vue
@@ -0,0 +1,49 @@
+<script setup lang="ts">
+</script>
+
+<template>
+    <section h-full grid="~ flow-col" place-items-start justify-items-center gap-x-3>
+        <!-- select payment -->
+        <div grid gap-y-5 mt-15>
+            <div text-4xl>Select a payment method</div>
+            <div grid="~ flow-col cols-[auto_auto_1fr] gap-x-2">
+                <input type="radio" checked>
+                <div i-carbon:piggy-bank place-self-center/>
+                <div text-lg> Pay by bank instantly and save 3%</div>
+            </div>
+
+
+            <div grid="~ flow-col cols-[auto_auto_1fr] gap-x-2">
+                <input type="radio" disabled>
+                <div i-carbon:money place-self-center/>
+                <div text-lg> Credit/Debit card</div>
+            </div>
+        </div>
+
+        <!-- product -->
+        <div grid gap-x-2 gap-y-4 mt-15>
+            <div grid="~ flow-col" gap-x-3>
+                <div place-self-center>
+                        <div text-xl>2023 Cadillac CT5</div>
+                        <div text-sm>Premium Luxury 4D</div>
+                        <div text-sm>Sedan Gas</div>
+                </div>
+                <img src="/assets/images/cadillac.png" alt="product" max-w-xs />
+            </div>
+
+            <div grid="~ flow-col">
+                <div i-carbon:vehicle-api />
+                <div text-sm>Full liability insured motor vehicles - Collisions and property coverage</div>
+            </div>
+
+            <button btn class="h-48px w-250px" place-self-center disabled>Next: Review</button>
+        </div>        
+    </section>
+</template>
+
+<route lang="yaml">
+meta:
+    requireAuth: true
+    layout: dashboard
+    title: Embedded
+</route>
