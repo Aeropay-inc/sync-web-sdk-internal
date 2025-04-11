aa48d0ab8615a30baa623e83e06a5d7e3118736f
jayon <jayon.niravel@aeropay.com>
2025-04-10 23:03:31 -0500

    Add sync sdk via tar ball

--- src/components.d.ts
+++ src/components.d.ts
@@ -10,7 +10,6 @@ declare module 'vue' {
   export interface GlobalComponents {
     RouterLink: typeof import('vue-router')['RouterLink']
     RouterView: typeof import('vue-router')['RouterView']
-    TheCounter: typeof import('./components/TheCounter.vue')['default']
     TheFooter: typeof import('./components/TheFooter.vue')['default']
     TheHeader: typeof import('./components/TheHeader.vue')['default']
     TheInput: typeof import('./components/TheInput.vue')['default']
