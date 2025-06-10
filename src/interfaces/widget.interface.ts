8ca40eb068a05d454c00edeb4be70a7c567e439e
jayon <jayon.niravel@aeropay.com>
2025-06-10 13:21:14 -0500

    renamed version

--- src/interfaces/widget.interface.ts
+++ src/interfaces/widget.interface.ts
@@ -11,7 +11,7 @@ export interface WidgetConfigType {
 }
 
 type AeroSyncWebVersionsKey = 'v110' | 'v111' | 'v204' | 'v205'
-type AeroSyncWebVersionsDisplay = 'v1.1.0' | 'v1.1.1' | 'v2.0.4' | 'v2.0.5'
+type AeroSyncWebVersionsDisplay = 'v1.1.0' | 'v1.1.1' | 'v2.0.4' | 'v2.1.0'
 
 export type Environment = 'qa' | 'staging' | 'sandbox' | 'production'
 export type EnvironmentV110 = 'dev' | 'staging' | 'sandbox' | 'production'
